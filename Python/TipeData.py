7039d9a6970fb8fad5b8bb84d874ea44d6d3f276
Nabil Maliqi Ramadhan <nabilmal.dev@gmail.com>
2026-08-08 06:40:55 +0700

    Add files via upload

---
+++ Python/TipeData.py
@@ -0,0 +1,17 @@
+# Tipe data itu ada 4 :
+#     - String ("")
+#     - Integer (int) / angka genap
+#     - Float (float) / angka ganjil
+#     - Boolean (and , or)
+
+nama = input("Masukan Nama : " )
+genap = int(input("Masukan Angka : " ))
+# ganjil = int(input("Masukan Angka : " ))
+
+# String , Integer dan float biasanya ada di inputan 
+# sedangkan boolean ada di percabangan atau perulangan 
+
+if nama == "Nabil" and genap == 21:
+    print("Si Ganteng")
+else: 
+    print("Lu siapa jir")
